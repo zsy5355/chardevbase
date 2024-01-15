8bdb86016cbf1ea1b3db71cef2f38a0a130c957c
郑松岩(平台开发) <zhengsongyan@BG-U2204-0003.t3caic.com>
2024-01-15 16:16:05 +0800

    new push

--- put.sh
+++ put.sh
@@ -1,5 +1,5 @@
 
 git add .
 git commit -m "new push"
-git push https://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@github.com/zsy5355/linux-source-code-analyze.git main
+git push https://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@github.com/zsy5355/chardevbase.git main
 
